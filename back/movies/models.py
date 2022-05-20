7872f9b855cf83c6c105d6469aba137623e24b8d
choi_jw <chlwlsdnr1000@gmail.com>
2022-05-20 17:35:12 +0900

    back0520

--- back/movies/models.py
+++ back/movies/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.TextField()
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    release_date = models.DateField()
+    vote_average = models.FloatField()
+    poster_path = models.CharField(max_length=200)
+
+class Article(models.Model):
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="articles")
+  title = models.CharField(max_length=100)
+  content = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments_user")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.TextField()
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
+
+class Movie(models.Model):
+    title = models.TextField()
+    overview = models.TextField()
+    genres = models.ManyToManyField(Genre)
+    vote_average = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    release_date = models.TextField()
+    poster_path = models.TextField()
