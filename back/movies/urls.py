526f2b9dbd0f25352d37cfdb8c65606e7c3a8f44
choi_jw <chlwlsdnr1000@gmail.com>
2022-05-25 11:16:11 +0900

    25

--- back/movies/urls.py
+++ back/movies/urls.py
@@ -6,5 +6,7 @@ app_name = 'movies'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.detail, name='detail'),
-
+    path('articles/<int:article_pk>/', views.article_detail),
+    path('articles/<int:article_pk>/comments/', views.comment_create),
+    path('comments/<int:comment_pk>/', views.comment_detail),
 ]
